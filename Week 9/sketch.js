1df50294129e98c3d70093a4a994b5b8e63b0419
diddlydang <90276493+diddlydang@users.noreply.github.com>
2021-11-01 13:22:21 -0600

    Add files via upload

---
+++ Week 9/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //
+
+  rect(90,70,210,190);
+  //
+ ellipse(200,170,150,180)
+rect(130,270,150,130)
+  rect(90,270,50,130)
+  rect(260,270,50,130)
+  circle(165,150,30)
+  circle(225,150,30)
+  triangle(190, 200,200,170,210,200)
+  text("Ireland Bowden",250,20,30)
+  text("Self-Portrait",100,20,30)
+}
